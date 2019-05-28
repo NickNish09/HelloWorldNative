011068ca6494c6c2c2033df62d394d61ea34624e
nicknish09 <nnmarques97@gmail.com>
2019-05-28 16:10:28 -0300

    criado arquivo pra testar wheel view

--- App.js
+++ App.js
@@ -9,7 +9,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Alert} from 'react-native';
 import ScratchView from 'react-native-scratch';
-import InfiniteScroll from 'react-native-wheel-view';
+import InfiniteScroll from './src/WheelView';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -65,20 +65,20 @@ export default class App extends Component<Props> {
             renderItem={({ item }) => <View style={styles.listItem}><Text style={styles.text}>{item.key}</Text></View>}
           />
         </View>
-        <ScratchView
-          id={1} // ScratchView id (Optional)
-          brushSize={32} // Default is 10% of the smallest dimension (width/height)
-          threshold={30} // Report full scratch after 70 percentage, change as you see fit. Default is 50
-          fadeOut={true} // Disable the fade out animation when scratch is done. Default is true
-          placeholderColor="#AAAAAA" // Scratch color while image is loading (or while image not present)
-          imageUrl="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Akali_9.jpg" // A url to your image (Optional)
-          resourceName="akali" // An image resource name (without the extension like '.png/jpg etc') in the native bundle of the app (drawble for Android, Images.xcassets in iOS) (Optional)
-          resizeMode="cover" // Resize the image to fit or fill the scratch view. Default is stretch
-          onImageLoadFinished={this.onImageLoadFinished} // Event to indicate that the image has done loading
-          onTouchStateChanged={this.onScratchTouchStateChanged} // Touch event (to stop a containing FlatList for example)
-          onScratchProgressChanged={this.onScratchProgressChanged} // Scratch progress event while scratching
-          onScratchDone={this.onScratchDone} // Scratch is done event
-        />
+        {/*<ScratchView*/}
+          {/*id={1} // ScratchView id (Optional)*/}
+          {/*brushSize={32} // Default is 10% of the smallest dimension (width/height)*/}
+          {/*threshold={30} // Report full scratch after 70 percentage, change as you see fit. Default is 50*/}
+          {/*fadeOut={true} // Disable the fade out animation when scratch is done. Default is true*/}
+          {/*placeholderColor="#AAAAAA" // Scratch color while image is loading (or while image not present)*/}
+          {/*imageUrl="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Akali_9.jpg" // A url to your image (Optional)*/}
+          {/*resourceName="akali" // An image resource name (without the extension like '.png/jpg etc') in the native bundle of the app (drawble for Android, Images.xcassets in iOS) (Optional)*/}
+          {/*resizeMode="cover" // Resize the image to fit or fill the scratch view. Default is stretch*/}
+          {/*onImageLoadFinished={this.onImageLoadFinished} // Event to indicate that the image has done loading*/}
+          {/*onTouchStateChanged={this.onScratchTouchStateChanged} // Touch event (to stop a containing FlatList for example)*/}
+          {/*onScratchProgressChanged={this.onScratchProgressChanged} // Scratch progress event while scratching*/}
+          {/*onScratchDone={this.onScratchDone} // Scratch is done event*/}
+        {/*/>*/}
       </View>
     );
   }
---
+++ src/WheelView.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import PropTypes from 'prop-types'
+
+export default class WheelView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+  }
+  render() {
+    return (
+      <View>
+        <Text>Oi</Text>
+      </View>
+    );
+  }
+}
